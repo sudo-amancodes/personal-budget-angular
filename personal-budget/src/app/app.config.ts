3df6bf8d612138470c858d26efbae5ac5b1c6ec1
AmanW-GitHub <amanwaghchoure2004@gmail.com>
2025-02-17 09:49:32 -0500

    Styling

--- personal-budget/src/app/app.config.ts
+++ personal-budget/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import {
   provideClientHydration,
@@ -12,6 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(),
   ],
 };
